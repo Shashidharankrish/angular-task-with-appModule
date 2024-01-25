72ab49e44494b5c29b2b8cb5a79a09448f0e1b8b
Shashidharan K <shashidharank@Shashidharans-MacBook-Pro.local>
2024-01-25 09:18:36 +0530

    api url updated

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: false,
-    API_URL: 'https://crudcrud.com/api/2d24b3dfaabf4a0e98a12f13f054c6ae',
+    API_URL: 'https://crudcrud.com/api/fdc9659286424a73bd7f8ace155e0345',
   };
   
