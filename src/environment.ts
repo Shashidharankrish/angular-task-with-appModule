5195c51ea67d1893937b4202496bae602b5d677d
Shashidharan K <shashidharank@Shashidharans-MacBook-Pro.local>
2024-01-26 08:33:53 +0530

    changed endpoint api

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: false,
-    API_URL: 'https://crudcrud.com/api/fdc9659286424a73bd7f8ace155e0345',
+    API_URL: 'https://crudcrud.com/api/60027cc9c17444239d9d6630c7c9496d',
   };
   
